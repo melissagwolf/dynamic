5178d1522bd9f412654502ba00f0d269b8266061
Daniel McNeish <dmcneish@asurite.asu.edu>
2023-12-05 10:44:16 -0700

    Documentation for example data

---
+++ R/Example-data.R
@@ -0,0 +1,26 @@
+#' Arabidopsis QTL data on gravitropism
+#'
+#' Simulated 5-point Likert item responses to 12 items for 500 people.
+#'
+#' @docType data
+#'
+#' @usage data(Example)
+#'
+#' @format ## 'Example'
+#' A data frame with 12 columns and 500 rows
+#' \describe{
+#' \item{X1}{Response to item 1}
+#' \item{X2}{Response to item 2}
+#' \item{X3}{Response to item 3}
+#' \item{X4}{Response to item 4}
+#' \item{X5}{Response to item 5}
+#' \item{X6}{Response to item 6}
+#' \item{X7}{Response to item 7}
+#' \item{X8}{Response to item 8}
+#' \item{X9}{Response to item 9}
+#' \item{X10}{Response to item 10}
+#' \item{X11}{Response to item 11}
+#' \item{X12}{Response to item 12}
+#' }
+#'
+"Example"
