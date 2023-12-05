d3b9aabf369f10d7c505c66ed04c70d16c703c10
Daniel McNeish <dmcneish@asurite.asu.edu>
2023-12-05 10:59:28 -0700

    update example data and documentation

--- R/Example-data.R
+++ R/Example-data.R
@@ -1,4 +1,4 @@
-#' Arabidopsis QTL data on gravitropism
+#' DFI example data
 #'
 #' Simulated 5-point Likert item responses to 12 items for 500 people.
 #'
@@ -6,21 +6,23 @@
 #'
 #' @usage data(Example)
 #'
+#' @keywords datasets
+#'
 #' @format ## 'Example'
 #' A data frame with 12 columns and 500 rows
 #' \describe{
-#' \item{X1}{Response to item 1}
-#' \item{X2}{Response to item 2}
-#' \item{X3}{Response to item 3}
-#' \item{X4}{Response to item 4}
-#' \item{X5}{Response to item 5}
-#' \item{X6}{Response to item 6}
-#' \item{X7}{Response to item 7}
-#' \item{X8}{Response to item 8}
-#' \item{X9}{Response to item 9}
-#' \item{X10}{Response to item 10}
-#' \item{X11}{Response to item 11}
-#' \item{X12}{Response to item 12}
+#' \item{X1}{Likert response to item 1}
+#' \item{X2}{Likert response to item 2}
+#' \item{X3}{Likert response to item 3}
+#' \item{X4}{Likert response to item 4}
+#' \item{X5}{Likert response to item 5}
+#' \item{X6}{Likert response to item 6}
+#' \item{X7}{Likert response to item 7}
+#' \item{X8}{Likert response to item 8}
+#' \item{X9}{Likert response to item 9}
+#' \item{X10}{Likert response to item 10}
+#' \item{X11}{Likert response to item 11}
+#' \item{X12}{Likert response to item 12}
 #' }
 #'
 "Example"
