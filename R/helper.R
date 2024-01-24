6f7aff031d52159834cae72b54efdf5ffe05c7a7
Daniel McNeish <dmcneish@asurite.asu.edu>
2024-01-24 15:33:42 -0700

    refine Likert function to use intermediate matrix instead of polychoric (save as separate function likertHB2 for now while testing)

--- R/helper.R
+++ R/helper.R
@@ -9,7 +9,7 @@
 #' @importFrom stats quantile qnorm cov2cor density rbeta rnorm runif
 #' @importFrom semTools bsBootMiss
 #' @importFrom MASS mvrnorm
-
+#' @importFrom GenOrd ordcont
 
 ############################################################################
 ########################### Multiple Functions #############################
@@ -3157,6 +3157,447 @@ data_likert <- function(model,data, n,reps){
   return(miss_dat)
 }
 
+##################################################
+################# likertHB2 ######################
+##################################################
+
+
+#function for misspecified model
+multi_fit_likert2 <- function(model,data, n,estimator,reps){
+
+  #Get clean model equation
+  mod <- cleanmodel(model)
+
+  factors <- lavaan::lavaanify(model, fixed.x=FALSE) %>%
+    dplyr::filter(lhs != rhs)%>%
+    dplyr::filter(op=="=~") %>%
+    dplyr::select(lhs) %>%
+    base::unique()
+
+  #Get model-implied matrix of input model
+  norm <- simstandard::sim_standardized_matrices(model)
+  normx<-norm$Correlations$R
+  l<-nrow(normx)-nrow(factors)
+  a<-normx[1:l,1:l]
+  diag(a)=1
+
+  #names of variable in model
+  names<-colnames(a)
+
+  #identify names of variables from dataset
+  data1<-data[,names]
+  #remove cases if all relevant variables are NA
+  data1<-data1[rowSums(is.na(data1)) != ncol(data1), ]
+
+  #rescale so that minimum value is always ==1
+  #needed to properly index computations below
+  #also needed to avoid 0s because there is multiplication involved
+  #will be scaled back at the end after loop indexes are not longer needed
+  d2<-matrix(sapply(data1, function(x) min(x, na.rm=T)-1), nrow=1, ncol=ncol(data1))
+  d3<-matrix(rep(d2,each=nrow(data1)), nrow=nrow(data1), ncol=ncol(data1))
+  colnames(d3)<-colnames(data1)
+  data1<-data1-d3
+
+  #create empty matrix for proportions in each category (g)
+  #GenOrd requires list (p) for each item
+  #t is thresholds for eventual discretization
+  g<-matrix(nrow=(max(data1,na.rm=T)-min(data1, na.rm=T)), ncol=ncol(data1))
+  p<-list()
+  t<-g
+
+  #loop through all variables in fitted model
+  for (i in 1:ncol(data1)){
+    #loop through all categories for each variable
+    for (h in min(data1[,i],na.rm=T):(max(data1[,i],na.rm=T)-1)){
+      #proportion of responses at or below each category, only count non-missing in the denominator
+      g[h,i]<-sum(table(data1[,i])[names(table(data1[,i]))<=h])/sum(!is.na(data1[,i]))
+      p[[i]]<-g[,i]
+      t[h,i]<-qnorm(c(g[h,i]))
+      dp<-as.data.frame(t)
+    }
+  }
+
+
+  #Assign column names so it's clear which thresholds go with which variable
+  colnames(g)<-colnames(data1)
+  colnames(dp)<-colnames(data1)
+  a2<-colnames(g)
+
+  #calculate target matrix of fitted model
+  z<-GenOrd::ordcont(marginal=p,Sigma=a)
+
+  #get estimates from target matrix
+  norm_model<-lavaan::cfa(model=mod, sample.cov=z$SigmaC, sample.nobs=n)
+
+  #Model statement for target matrix estimates
+  n_model<-cfa_lavmod(norm_model)
+
+  #find misspecifications on target matrix scale
+  misspec_dgm <- DGM_Multi_HB(n_model)
+
+  n <- min(n,2000)
+
+  #Set seed
+  set.seed(269854)
+
+  #Number of reps (default is 500 and shouldn't be changed by empirical researchers)
+  r <- reps
+
+  all_data_misspec <- purrr::map(misspec_dgm,~simstandard::sim_standardized(m=.,n=n*r,
+                                                                            latent=FALSE,errors=FALSE))
+
+  #Grab data level of the list
+  #data <- purrr::map(misspec_data,2)
+
+  #loop through misspecifications
+  for (x in 1:length(misspec_dgm))
+  {
+    for (i in 1:ncol(dp))
+    {# first loop transforms highest category
+      u<- as.numeric(sum(!is.na(dp[,i])))
+      all_data_misspec[[x]]<-all_data_misspec[[x]] %>%
+        dplyr::mutate(!!a2[i] := case_when(
+          !!rlang::sym(a2[i])  <= dp[1,i] ~100,
+          !!rlang::sym(a2[i]) >   dp[u,i] ~100*(u+1),
+          TRUE ~ !!rlang::sym(a2[i]))
+        )
+      #second loop transforms all lower categories
+      if(sum(!is.na(dp[,i])) > 1){
+        for (j in 1:sum(!is.na(dp[,i]))) {
+          all_data_misspec[[x]]<-all_data_misspec[[x]] %>%
+            dplyr::mutate(!!a2[i] := case_when(
+              between( !!rlang::sym(a2[i]) , dp[j,i], dp[j+1,i]) ~ as.numeric(100*(j+1)),
+              TRUE~ !!rlang::sym(a2[i]))
+            )
+        }
+      }
+    }
+    #loops multiply by 100 to avoid overwriting MVN data with values above 1
+    #divide by 100 to put things back onto Likert metric
+    all_data_misspec[[x]]<-all_data_misspec[[x]]/100
+  }
+
+  #Create indicator to split into 500 datasets for 500 reps
+  rep_id_misspec <- rep(1:r,n)
+
+  #Combine indicator with dataset for each element in list
+  dat_rep_misspec <- purrr::map(all_data_misspec,~cbind(.,rep_id_misspec))
+
+  #Group and list
+  misspec_data <- purrr::map(dat_rep_misspec,~group_by(.,rep_id_misspec) %>%
+                               tidyr::nest())
+
+  #Grab data level of the list
+  data_m <- purrr::map(misspec_data,2)
+
+  #if using ULS or one of its variants, the following must be specified for standard errors: se="standard"
+  se<-ifelse(startsWith(estimator,"ULS"),"standard","none")
+  if(endsWith(estimator,"MVS") | endsWith(estimator,"V")) {
+    ind<-c("srmr","rmsea.scaled","cfi.scaled")
+  } else if(endsWith(estimator,"M") | endsWith(estimator,"R")) {
+    ind<-c("srmr","rmsea.robust","cfi.robust")
+  }  else {
+    ind<-c("srmr","rmsea","cfi")
+  }
+
+  #Run 500 cfa for each element in the list
+  misspec_cfa <- purrr::map(data_m, function(x) purrr::map(x, function(y) lavaan::cfa(model=mod,
+                                                                                      estimator=estimator,
+                                                                                      data=y,
+                                                                                      std.lv=TRUE,
+                                                                                      se=se,
+                                                                                      check.gradient=FALSE,
+                                                                                      check.post=FALSE,
+                                                                                      check.vcov=FALSE,
+                                                                                      control=list(rel.tol=.001))))
+
+  #Extract fit stats from each rep (list) into a data frame and clean using nested lapply
+  #map_dfr returns data frame instead of list
+  #for each misspecification level (in the list), access the lavaan objects (x)
+  #and extract the fit stats (y) - and return as a df
+  misspec_fit_sum <- purrr::map(misspec_cfa, function(x) purrr::map_dfr(x, function(y) lavaan::fitMeasures(y, ind)) %>%
+                                  `colnames<-`(c("SRMR_M","RMSEA_M","CFI_M")) %>%
+                                  dplyr::mutate(Type_M="Misspecified"))
+
+  set.seed(NULL)
+
+  return(misspec_fit_sum)
+
+}
+
+## Function for correct model
+
+true_fit_multi_likert2 <- function(model,data,n,estimator,reps){
+
+  #Get clean model equation
+  mod <- cleanmodel(model)
+
+  true_dgm <- model
+
+  #Get model-implied matrix of input model
+  norm <- simstandard::sim_standardized_matrices(true_dgm)
+  normx<-norm$Correlations$R
+  l<-nrow(normx)-nrow(factors)
+  a<-normx[1:l,1:l]
+  diag(a)=1
+
+  #names of variable in model
+  names<-colnames(a)
+
+  #identify names of variables from dataset
+  data1<-data[,names]
+  #remove cases if all relevant variables are NA
+  data1<-data1[rowSums(is.na(data1)) != ncol(data1), ]
+
+  #rescale so that minimum value is always ==1
+  #needed to properly index computations below
+  #also needed to avoid 0s because there is multiplication involved
+  #will be scaled back at the end after loop indexes are not longer needed
+  d2<-matrix(sapply(data1, function(x) min(x, na.rm=T)-1), nrow=1, ncol=ncol(data1))
+  d3<-matrix(rep(d2,each=nrow(data1)), nrow=nrow(data1), ncol=ncol(data1))
+  colnames(d3)<-colnames(data1)
+  data1<-data1-d3
+
+  #create empty matrix for proportions in each category (g)
+  #GenOrd requires list (p) for each item
+  #t is thresholds for eventual discretization
+  g<-matrix(nrow=(max(data1,na.rm=T)-min(data1, na.rm=T)), ncol=ncol(data1))
+  p<-list()
+  t<-g
+
+  #loop through all variables in fitted model
+  for (i in 1:ncol(data1)){
+    #loop through all categories for each variable
+    for (h in min(data1[,i],na.rm=T):(max(data1[,i],na.rm=T)-1)){
+      #proportion of responses at or below each category, only count non-missing in the denominator
+      g[h,i]<-sum(table(data1[,i])[names(table(data1[,i]))<=h])/sum(!is.na(data1[,i]))
+      p[[i]]<-g[,i]
+      t[h,i]<-qnorm(c(g[h,i]))
+      dp<-as.data.frame(t)
+    }
+  }
+
+
+  #Assign column names so it's clear which thresholds go with which variable
+  colnames(g)<-colnames(data1)
+  colnames(dp)<-colnames(data1)
+  a2<-colnames(g)
+
+  #calculate target matrix of fitted model
+  z<-GenOrd::ordcont(marginal=p,Sigma=a)
+
+  #get estimates from target matrix
+  norm_model<-lavaan::cfa(model=mod, sample.cov=z$SigmaC, sample.nobs=n)
+
+  #Model statement for target matrix estimates
+  n_model<-cfa_lavmod(norm_model)
+
+  #Use max sample size of 10000
+  n <- base::min(n,2000)
+
+  #Set Seed
+  set.seed(326267)
+
+  #Number of reps (default is 500 and shouldn't be changed by empirical researchers)
+  r <- reps
+
+  #Simulate one large dataset
+  all_data_true <- simstandard::sim_standardized(m=n_model,n = n*r,
+                                                 latent = FALSE,
+                                                 errors = FALSE)
+
+  for (i in 1:ncol(dp))
+  {# first loop transforms highest category
+    u<- as.numeric(sum(!is.na(dp[,i])))
+    all_data_true<-all_data_true %>%
+      dplyr::mutate(!!a2[i] := case_when(
+        !!rlang::sym(a2[i])  <= dp[1,i] ~100,
+        !!rlang::sym(a2[i]) >   dp[u,i] ~100*(u+1),
+        TRUE ~ !!rlang::sym(a2[i]))
+      )
+    #second loop transforms all lower categories
+    if(sum(!is.na(dp[,i])) > 1){
+      for (j in 1:sum(!is.na(dp[,i]))) {
+        all_data_true<-all_data_true %>%
+          dplyr::mutate(!!a2[i] := case_when(
+            between( !!rlang::sym(a2[i]) , dp[j,i], dp[j+1,i]) ~ as.numeric(100*(j+1)),
+            TRUE~ !!rlang::sym(a2[i]))
+          )
+      }
+    }
+  }
+  #loops multiply by 100 to avoid overwriting MVN data with values above 1
+  #divide by 100 to put things back onto Likert metric
+  all_data_true<-all_data_true/100
+
+  rep_id_true <- base::rep(1:r,n)
+
+  #Combine indicator with dataset
+  dat_rep_true <- base::cbind(all_data_true,rep_id_true)
+
+  #Group and list
+  data_t <- dat_rep_true %>%
+    dplyr::group_by(rep_id_true) %>%
+    tidyr::nest() %>%
+    base::as.list()
+
+  #if using ULS or one of its variants, the following must be specified for standard errors: se="standard"
+  se<-ifelse(startsWith(estimator,"ULS"),"standard","none")
+  if(endsWith(estimator,"MVS") | endsWith(estimator,"V")) {
+    ind<-c("srmr","rmsea.scaled","cfi.scaled")
+  } else if(endsWith(estimator,"M") | endsWith(estimator,"R")) {
+    ind<-c("srmr","rmsea.robust","cfi.robust")
+  }  else {
+    ind<-c("srmr","rmsea","cfi")
+  }
+
+  #Run 500 cfa
+  true_cfa <- purrr::map(data_t$data,function(x) lavaan::cfa(model=mod,
+                                                             estimator=estimator,
+                                                             data=x,
+                                                             std.lv=TRUE,
+                                                             se=se,
+                                                             check.gradient=FALSE,
+                                                             check.post=FALSE,
+                                                             check.vcov=FALSE,
+                                                             control=list(rel.tol=.001)))
+
+  true_fit_sum <- purrr::map_dfr(true_cfa,~lavaan::fitMeasures(., ind)) %>%
+    `colnames<-`(c("SRMR_T","RMSEA_T","CFI_T")) %>%
+    dplyr::mutate(Type_T="True")
+
+  set.seed(NULL)
+
+  return(true_fit_sum)
+}
+
+multi_df_likert2 <- function(model,data,n,estimator,reps){
+
+  #Use max sample size of 2000
+  n <- min(n,2000)
+
+  #Get fit stats for misspecified model
+  misspec_fit <- multi_fit_likert2(model,data,n,estimator,reps)
+
+  #Get fit stats for correctly specified model
+  true_fit <- true_fit_multi_likert2(model,data,n,estimator,reps)
+
+  #Produce final table by level
+  Table <- purrr::map(misspec_fit,~cbind(.,true_fit))
+
+  #Final table
+  return(Table)
+}
+
+data_likert2 <- function(model,data, n,reps){
+
+  mod <- cleanmodel(model)
+
+  true_dgm <- model
+
+  #Get model-implied matrix of input model
+  norm <- simstandard::sim_standardized_matrices(true_dgm)
+  normx<-norm$Correlations$R
+  l<-nrow(normx)-nrow(factors)
+  a<-normx[1:l,1:l]
+  diag(a)=1
+
+  #names of variable in model
+  names<-colnames(a)
+
+  #identify names of variables from dataset
+  data1<-data[,names]
+  #remove cases if all relevant variables are NA
+  data1<-data1[rowSums(is.na(data1)) != ncol(data1), ]
+
+  #rescale so that minimum value is always ==1
+  #needed to properly index computations below
+  #also needed to avoid 0s because there is multiplication involved
+  #will be scaled back at the end after loop indexes are not longer needed
+  d2<-matrix(sapply(data1, function(x) min(x, na.rm=T)-1), nrow=1, ncol=ncol(data1))
+  d3<-matrix(rep(d2,each=nrow(data1)), nrow=nrow(data1), ncol=ncol(data1))
+  colnames(d3)<-colnames(data1)
+  data1<-data1-d3
+
+  #create empty matrix for proportions in each category (g)
+  #GenOrd requires list (p) for each item
+  #t is thresholds for eventual discretization
+  g<-matrix(nrow=(max(data1,na.rm=T)-min(data1, na.rm=T)), ncol=ncol(data1))
+  p<-list()
+  t<-g
+
+  #loop through all variables in fitted model
+  for (i in 1:ncol(data1)){
+    #loop through all categories for each variable
+    for (h in min(data1[,i],na.rm=T):(max(data1[,i],na.rm=T)-1)){
+      #proportion of responses at or below each category, only count non-missing in the denominator
+      g[h,i]<-sum(table(data1[,i])[names(table(data1[,i]))<=h])/sum(!is.na(data1[,i]))
+      p[[i]]<-g[,i]
+      t[h,i]<-qnorm(c(g[h,i]))
+      dp<-as.data.frame(t)
+    }
+  }
+
+
+  #Assign column names so it's clear which thresholds go with which variable
+  colnames(g)<-colnames(data1)
+  colnames(dp)<-colnames(data1)
+  a2<-colnames(g)
+
+  #calculate target matrix of fitted model
+  z<-GenOrd::ordcont(marginal=p,Sigma=a)
+
+  #get estimates from target matrix
+  norm_model<-lavaan::cfa(model=mod, sample.cov=z$SigmaC, sample.nobs=n)
+
+  #Model statement for target matrix estimates
+  n_model<-cfa_lavmod(norm_model)
+
+  #Use max sample size of 10000
+  n <- base::min(n,2000)
+
+  #Set Seed
+  set.seed(326267)
+
+  #Number of reps (default is 500 and shouldn't be changed by empirical researchers)
+  r <- reps
+
+  #Simulate one large dataset
+  all_data_true <- simstandard::sim_standardized(m=n_model,n = n*r,
+                                                 latent = FALSE,
+                                                 errors = FALSE)
+
+  for (i in 1:ncol(dp))
+  {# first loop transforms highest category
+    u<- as.numeric(sum(!is.na(dp[,i])))
+    all_data_true<-all_data_true %>%
+      dplyr::mutate(!!a2[i] := case_when(
+        !!rlang::sym(a2[i])  <= dp[1,i] ~100,
+        !!rlang::sym(a2[i]) >   dp[u,i] ~100*(u+1),
+        TRUE ~ !!rlang::sym(a2[i]))
+      )
+    #second loop transforms all lower categories
+    if(sum(!is.na(dp[,i])) > 1){
+      for (j in 1:sum(!is.na(dp[,i]))) {
+        all_data_true<-all_data_true %>%
+          dplyr::mutate(!!a2[i] := case_when(
+            between( !!rlang::sym(a2[i]) , dp[j,i], dp[j+1,i]) ~ as.numeric(100*(j+1)),
+            TRUE~ !!rlang::sym(a2[i]))
+          )
+      }
+    }
+  }
+  #loops multiply by 100 to avoid overwriting MVN data with values above 1
+  #divide by 100 to put things back onto Likert metric
+  all_data_true<-all_data_true/100
+
+  miss_dat<-list()
+
+  miss_dat$sim<-all_data_true
+  miss_dat$orig<-data1
+
+  return(miss_dat)
+}
 
 ##################################################
 ################# likertOne ######################
